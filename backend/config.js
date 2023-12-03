57d6d068cb6de85cbad7259871a5216e81e472dd
Alex Segura <62962671+Alex-003@users.noreply.github.com>
2023-12-03 17:39:38 +0000

    fixed bookModel.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://root:root@bookstoremern.ku7qla3.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://root:root@bookstoremern.ku7qla3.mongodb.net/books-collection?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,56 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "../backend/models/bookModel.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome");
+});
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Route to Save a new Book
+app.post("/books", async (request, response) => {
+  try {
+    console.log("Request body:", request.body);
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message:
+          "Fill all required fields: title, author, publisher, publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (err) {
+    console.log(err.message);
+    response.status(500).send({ message: err.message });
+  }
+});
+
+// Route for Get All books from the database
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json(books);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
 });
 
 mongoose
