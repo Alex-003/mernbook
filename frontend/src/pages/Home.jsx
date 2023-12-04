c8255af2a18abea2a48fcb34595984fee9944f2f
Alex Segura <62962671+Alex-003@users.noreply.github.com>
2023-12-04 20:24:43 +0000

    showbooks functional

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
 import { MdOutlineAddBox } from "react-icons/md";
+import { BsInfoCircle } from "react-icons/bs";
+import { AiOutlineEdit } from "react-icons/ai";
+import { MdOutlineDelete } from "react-icons/md";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
@@ -41,17 +44,50 @@ const Home = () => {
               <th className="border-slate-600 rounded-md max-md:hidden">
                 Author
               </th>
+              <th className="border-slate-600 rounded-md max-md:hidden">
+                Publish Year
+              </th>
+              <th className="border-slate-600 rounded-md">Operations</th>
             </tr>
           </thead>
           <tbody>
-            {/* Populate the table body with data */}
             {books.map((book, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{book.title}</td>
-                <td>{book.author}</td>
+              <tr key={book._id} className="h-8">
+                <td className="border border-slate-700 rounded-md text-center">
+                  {index + 1}
+                </td>
+
+                <td className="border border-slate-700 rounded-md text-center">
+                  {book.title}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                  {book.author}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                  {book.publishYear}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                  <div className="flex justify-center gap-x-4">
+                    <Link to={`/books/details/${book._id}`}>
+                      <BsInfoCircle className="text-2xl text-green-800"></BsInfoCircle>
+                    </Link>
+                    <Link to={`/books/edit/${book._id}`}>
+                      <AiOutlineEdit className="text-2xl text-yellow-600"></AiOutlineEdit>
+                    </Link>
+                    <Link to={`/books/delete/${book._id}`}>
+                      <MdOutlineDelete className="text-2xl text-red-600"></MdOutlineDelete>
+                    </Link>
+                  </div>
+                </td>
               </tr>
             ))}
+            {books.length === 0 && (
+              <tr>
+                <td colSpan="5" className="text-center">
+                  No books available.
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       )}
